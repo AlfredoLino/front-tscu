542a3a264e4d2de09de91f1a439ab4f567aa3ae3
Alfredo Lino Mendoza <alfredolinomendoza@gmail.com>
2021-05-18 01:47:54 -0500

    Formulario para nuevo informe

--- src/components/Bitacora.tsx
+++ src/components/Bitacora.tsx
@@ -1,23 +1,41 @@
+import { Button, makeStyles } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import React from 'react';
-import { server } from '../ngrok_server';
 import "../styles/Bitacora.css"
+
 
 interface BitacoraInterface {
     photo ?: string
 }
 
-const Bitacora : React.FC<BitacoraInterface> = ( {photo} ) : JSX.Element => {
+const useStyles = makeStyles({
+    root: {
+        background : 'linear-gradient(90deg, #02475e, #fefecc)',
+        fontWeight : 'bold',
+        width : "75%",
+        display : "block",
+        margin : "auto"
 
+    }
+})
+
+const Bitacora : React.FC<BitacoraInterface> = ( {photo} ) : JSX.Element => {
+    const classes = useStyles()
     const date = new Date().toISOString()
     return (
         <>
         <h1 className = "title-bitacora">Bitacora: </h1>
+        <form>
+            <TextField fullWidth className = "input-informe"  variant = "outlined" label = "Nuevo informe" multiline rows = {4} />
+            <Button className = "submit-form" type = "submit" variant = "contained" >Crear Informe</Button>
+        </form>
+        
         <ul>
             <li>
                 <article className = "informe">
                     <div className = "content-informe">
 
-                        <header>
+                        <header className = "header-informe">
                             <div className = "photo-name-bitacora">
                                 <img className = "photo-informe" src={photo} alt="Foto de perfil" />
                                 <h2>
@@ -48,7 +66,7 @@ const Bitacora : React.FC<BitacoraInterface> = ( {photo} ) : JSX.Element => {
                 <article className = "informe">
                     <div className = "content-informe">
 
-                        <header>
+                        <header className = "header-informe">
                             <div className = "photo-name-bitacora">
                                 <img className = "photo-informe" src={photo} alt="Foto de perfil" />
                                 <h2>
